3d5f5fa8b61abbb4a5be35b957aa61ecf34d4017
calvinfranca <calvin.franca@gmail.com>
2020-11-02 22:36:14 -0300

    novo cenario

---
+++ Assets/Popup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text textoNPC;
+    public string texto;
+    public GameObject painel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0;
+            textoNPC.text = texto;
+            painel.SetActive(true);
+
+        };
+
+    }
+    public void ExitText()
+    {
+        Time.timeScale = 1;
+        painel.SetActive(false);
+        GetComponent<Collider>().isTrigger = false;
+        
+
+    }
+}
--- Assets/scripts/Cura.cs
+++ Assets/scripts/Cura.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cura : MonoBehaviour
+public class Cura : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
